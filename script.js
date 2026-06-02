b40fd8bf71408990c926608e6425366a53d98e37
soujanyamohanty11-ToT <soujanya.mohanty12@gmail.com>
2026-06-02 16:37:32 +0530

    PO attainment page

--- script.js
+++ script.js
@@ -47,3 +47,81 @@ new Chart(lineCtx, {
         responsive: true //makes the chart automatically resize to fit its container when the browser window or screen size changes.
     }
 });
+const math1 = [82,76,91,69,74,80];
+const physics1 = [70,85,88,75,80,90];
+const spm1 = [88,92,90,84,95,91];
+
+const math2 = [75,70,82,68,73,79];
+const physics2 = [81,78,90,74,80,86];
+const spm2 = [90,93,89,84,95,91];
+
+let chart = new Chart(
+    document.getElementById("poChart"),
+    {
+        type: "bar",
+        data: {
+            labels: ["PO1","PO2","PO3","PO4","PO5","PO6"],
+            datasets: [{
+                label: "PO Attainment (%)",
+                data: math1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    }
+);
+
+function updateChart(){
+
+    let sem = document.getElementById("semester").value;
+    let sub = document.getElementById("subject").value;
+    let type = document.getElementById("chartType").value;
+
+    let values = math1;
+
+    if(sem == "Semester 1" && sub == "Applied Mathematics I"){
+        values = math1;
+    }
+
+    if(sem == "Semester 1" && sub == "Engineering Physics"){
+        values = physics1;
+    }
+
+    if(sem == "Semester 1" && sub == "Structured Programming Methodology"){
+        values = spm1;
+    }
+
+    if(sem == "Semester 2" && sub == "Applied Mathematics I"){
+        values = math2;
+    }
+
+    if(sem == "Semester 2" && sub == "Engineering Physics"){
+        values = physics2;
+    }
+
+    if(sem == "Semester 2" && sub == "Structured Programming Methodology"){
+        values = spm2;
+    }
+
+    chart.destroy();
+
+    chart = new Chart(
+        document.getElementById("poChart"),
+        {
+            type: type,
+            data: {
+                labels: ["PO1","PO2","PO3","PO4","PO5","PO6"],
+                datasets: [{
+                    label: "PO Attainment (%)",
+                    data: values
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    );
+}
